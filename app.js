0eefc610c9468a3a68c6f3dfff53916b3e345370
sujithbabu1971 <sujithkp@yahoo.com>
2017-07-28 19:02:30 +0530

    Setting the template  view engine +// By default it seems to be ejs anyway +// Refer template engine section in https://github.com/expressjs/express/wiki +// for all available template engines.

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var port = process.env.PORT||4000;
 // This folder is the one from the static files are served. 
 // By default, the index.html is the index file
 app.use(exp.static(__dirname+ "/public"));
-// Setting the template engine
-app.set("view engine", exp);
+// Setting the template  view engine
+// By default it seems to be ejs anyway
+// Refer template engine section in https://github.com/expressjs/express/wiki
+// for all available template engines.
+app.set("view engine", "ejs");
 app.listen(port, listenFn);
 
 // . is neede for relative path to the current folder.
